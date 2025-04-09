03a6b391672f69b8e9a6d1c686bd8971544894bb
Lisa Vercruysse <Lisa_Vercruysse@hotmail.com>
2025-04-09 12:14:17 +0200

    Setting up project with needed dependancies

---
+++ EWDJ_examenopdracht_IT_conferentie/src/main/java/com/examenopdracht_ITconferentie/EwdjExamenopdrachtItConferentieApplication.java
@@ -0,0 +1,13 @@
+package com.examenopdracht_ITconferentie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwdjExamenopdrachtItConferentieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwdjExamenopdrachtItConferentieApplication.class, args);
+	}
+
+}
---
+++ EWDJ_examenopdracht_IT_conferentie/src/test/java/com/examenopdracht_ITconferentie/EwdjExamenopdrachtItConferentieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examenopdracht_ITconferentie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwdjExamenopdrachtItConferentieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
